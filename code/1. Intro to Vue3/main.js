14f2b8feab6e6f03e171cf710be3b24ea2c0ed28
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-18 18:50:14 +0500

    b1_Les5 изучен v-for

--- code/1. Intro to Vue3/main.js
+++ code/1. Intro to Vue3/main.js
@@ -4,6 +4,13 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             inventory: 100,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'},
+            ],
+            sizes: ['XL', '2XL', '3XL']
         }
     }
 })
