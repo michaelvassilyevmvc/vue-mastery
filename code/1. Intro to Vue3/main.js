f3f9ebb786946a1d7b060d10a15b12bd0d449e28
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-18 19:36:41 +0500

    b1_Les8 Изучены вычисляемые свойства

--- code/1. Intro to Vue3/main.js
+++ code/1. Intro to Vue3/main.js
@@ -2,25 +2,36 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
             inventory: 100,
             onSale: true,
-            inStock:true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
             ],
             sizes: ['XL', '2XL', '3XL'],
-            cart: 0
+            cart: 0,
+            brand: 'Vue Mastery',
+        }
+    },
+    computed: {
+        title() {
+            return `${this.brand} ${this.product}`;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage;
+        updateVariant(index) {
+            this.selectedVariant = index;
         },
         decrementFromCart() {
             this.cart -= 1;
