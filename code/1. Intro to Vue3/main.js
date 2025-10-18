9b0b3f30545f1cf91f6a4e3487bd47815d2bb29a
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-18 19:22:40 +0500

    b1_Les7 изучено связывание стилей и классов

--- code/1. Intro to Vue3/main.js
+++ code/1. Intro to Vue3/main.js
@@ -5,6 +5,7 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             inventory: 100,
             onSale: true,
+            inStock:true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
