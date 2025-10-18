0d6eefc32ceac9b6293d34b5b833cade2d3fe20c
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-18 18:59:15 +0500

    b1_Les6 изучена привязка к событиям

--- code/1. Intro to Vue3/main.js
+++ code/1. Intro to Vue3/main.js
@@ -7,10 +7,19 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green'},
-                {id: 2235, color: 'blue'},
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
             ],
-            sizes: ['XL', '2XL', '3XL']
+            sizes: ['XL', '2XL', '3XL'],
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
         }
     }
 })
