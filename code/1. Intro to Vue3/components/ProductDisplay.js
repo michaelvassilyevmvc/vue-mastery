d3fddb78f48852e5db73c5034b2329332350c28a
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-19 07:45:27 +0500

    b1_Les9end изучена инкапсуляция кода в отдельный объект

---
+++ code/1. Intro to Vue3/components/ProductDisplay.js
@@ -0,0 +1,87 @@
+app.component('product-display', {
+    template:
+        `
+          <div class="product-display">
+            <div class="product-container">
+              <div class="product-image">
+                <img :class="{'out-of-stock-img':!inStock}" :src="image" alt="">
+              </div>
+              <div class="product-info">
+                <h1>{{ title }}</h1>
+                <p v-if="inventory > 10">In Stock</p>
+                <p v-else-if="inventory <= 10 && inventory > 0">Almost sold</p>
+                <p v-else>Out of Stock</p>
+                
+                <p>Shipping: {{shipping}}</p>
+                
+                <p v-if="onSale">onSale</p>
+                <ul>
+                  <li v-for="detail in details">{{ detail }}</li>
+                </ul>
+                <div v-for="(variant, index) in variants"
+                     :key="variant.id"
+                     @mouseover="updateVariant(index)"
+                     class="color-circle"
+                     :style="{backgroundColor: variant.color}"
+                >
+                </div>
+                <button :disabled="!inStock"
+                        :class="{disabledButton: !inStock}"
+                        class="button" @click="addToCart">Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        `,
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    data() {
+        return {
+            product: 'Socks',
+            selectedVariant: 0,
+            inventory: 100,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
+            ],
+            sizes: ['XL', '2XL', '3XL'],
+            brand: 'Vue Mastery',
+        }
+    },
+    computed: {
+        title() {
+            return this.onSale ? `${this.brand} ${this.product}` : '';
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
+        },
+        shipping(){
+            if (this.premium) {
+                return 'Free';
+            }
+            return 2.99;
+        }
+
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateVariant(index) {
+            this.selectedVariant = index;
+        },
+        decrementFromCart() {
+            this.cart -= 1;
+        },
+    }
+
+})
--- code/1. Intro to Vue3/main.js
+++ code/1. Intro to Vue3/main.js
@@ -1,40 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
-            selectedVariant: 0,
-            inventory: 100,
-            onSale: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
-                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
-            ],
-            sizes: ['XL', '2XL', '3XL'],
             cart: 0,
-            brand: 'Vue Mastery',
+            premium: true
         }
     },
-    computed: {
-        title() {
-            return this.onSale ? `${this.brand} ${this.product}` : '';
-        },
-        image() {
-            return this.variants[this.selectedVariant].image;
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity;
-        }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
-        },
-        decrementFromCart() {
-            this.cart -= 1;
-        },
-    }
+    methods: {}
 })
