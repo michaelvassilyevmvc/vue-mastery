dad6ab090a4441531601284a4db79e74876aa3fc
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-20 17:01:25 +0500

    b4_Les4end Отображен список заполненных данных и реализована функция отметки выполненных

--- code/c4_pinia_fund/todo-app/src/components/TodoList.vue
+++ code/c4_pinia_fund/todo-app/src/components/TodoList.vue
@@ -1,10 +1,24 @@
 <template>
+  <div v-for="todo in todoList" :key="todo.id" class="item">
+    <div class="content">
+      <span :class="{completed: todo.completed}">{{ todo.item }}</span>
+      <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import {useTodoListStore} from "@/stores/todoList.js";
+import {storeToRefs} from "pinia";
 
+const store = useTodoListStore();
+const {todoList} = storeToRefs(store);
+const {toggleCompleted} = store
 </script>
 
 <style scoped>
+.completed {
+  text-decoration: line-through;
+}
 
 </style>
--- code/c4_pinia_fund/todo-app/src/stores/todoList.js
+++ code/c4_pinia_fund/todo-app/src/stores/todoList.js
@@ -11,6 +11,12 @@ export const useTodoListStore = defineStore("todoList", {
         },
         deleteTodo(itemId) {
             this.todoList = this.todoList.filter(item => item.id !== item.id)
+        },
+        toggleCompleted(idToFind) {
+            const todo = this.todoList.find((obj) => obj.id === idToFind);
+            if (todo) {
+                todo.completed = !todo.completed;
+            }
         }
     }
 })
