03e95db80091d77698f942e28a7cf97eb857a6b3
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-20 13:25:29 +0500

    b3_Les6end Изучен useRouter и программный переход на нужный компонент

--- code/2. RealWorld Vue3/real-world-vue/src/views/event/Register.vue
+++ code/2. RealWorld Vue3/real-world-vue/src/views/event/Register.vue
@@ -1,13 +1,20 @@
 <script setup>
 import {ref, onMounted} from 'vue'
 import EventService from "@/services/EventService.js";
+import {useRouter} from "vue-router";
 
 const props = defineProps(["event"])
+const router = useRouter();
+
+const register = () => {
+  router.push({name: 'event-details'})
+}
 
 </script>
 
 <template>
   <p>Register for event here</p>
+  <button @click="register">Register Me!</button>
 </template>
 <style lang="scss" scoped>
 
