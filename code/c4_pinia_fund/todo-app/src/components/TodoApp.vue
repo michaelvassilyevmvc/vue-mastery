08b9603f2857230361fe47ea95e399e8fbbebc31
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-20 16:49:39 +0500

    b4_Les3end Создан проект и настроен хранилище и заполнение данными хранилища через компонент с формой

---
+++ code/c4_pinia_fund/todo-app/src/components/TodoApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="todo-app">
+    <h1>To Do List</h1>
+    <todo-form />
+    <todo-list />
+  </div>
+</template>
+
+<script setup>
+
+import TodoForm from "@/components/TodoForm.vue";
+import TodoList from "@/components/TodoList.vue";
+</script>
+
+<style  scoped>
+
+</style>
